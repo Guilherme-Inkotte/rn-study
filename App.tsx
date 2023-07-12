0a885fbf9b1d0adfefa7550b062d2c7f8a003bbe
Guilherme Inkotte <guilherme.inkotte@dentsu.com>
2023-07-12 16:57:56 -0300

    feat(analytics): added App Center Analytics and Crashes test

--- App.tsx
+++ App.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import Analytics from 'appcenter-analytics';
+import Crashes from 'appcenter-crashes';
 
 function App(): JSX.Element {
-  return <Text>RN Study</Text>;
+  return (
+    <View
+      style={{
+        backgroundColor: '#FFF',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>RN Study</Text>
+      <TouchableOpacity
+        style={{
+          borderColor: '#000',
+          borderWidth: 1,
+          borderRadius: 4,
+          padding: 4,
+          marginBottom: 8,
+        }}
+        onPress={() => Analytics.trackEvent('test')}>
+        <Text>Test App Center Analytics</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          borderColor: '#000',
+          borderWidth: 1,
+          borderRadius: 4,
+          padding: 4,
+        }}
+        onPress={() => Crashes.generateTestCrash()}>
+        <Text>Test App Center Crash</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 export default App;
